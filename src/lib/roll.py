b32b763ab372c3dbf8727c4c7703aac81ea02cf7
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-01-24 17:53:30 -0500

    privatized certain rolls

--- src/lib/roll.py
+++ src/lib/roll.py
@@ -6,19 +6,19 @@ def roll_standard(modifier: int = 0) -> int:
 def roll_stress(botch_dice: int = 1, modifier: int = 0) -> int | bool:
     die_roll = random.randint(0, 9)
     if die_roll == 0:
-        return botch_roll(botch_dice, modifier)
+        return _botch_roll(botch_dice, modifier)
     elif die_roll == 1:
-        return explode_stress()+modifier
+        return _explode_stress()+modifier
     else:
         return die_roll+modifier
 
-def explode_stress():
+def _explode_stress():
     die_roll = random.randint(1, 10)
     if die_roll == 1:
-        die_roll = explode_stress()
+        die_roll = _explode_stress()
     return 2*die_roll
 
-def botch_roll(botch_dice_num: int, modifier: int) -> bool | int:
+def _botch_roll(botch_dice_num: int, modifier: int) -> bool | int:
     dice_rolls = [random.randint(0, 9) == 0 for i in range(botch_dice_num)]
     if any(dice_rolls):
         return False
