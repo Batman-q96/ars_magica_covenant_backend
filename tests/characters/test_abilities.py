e8f178846ce2782450127f93cad190c8e1c16828
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-01-25 07:13:10 -0500

    rename

--- tests/characters/test_abilities.py
+++ tests/characters/test_abilities.py
@@ -1,39 +1,39 @@
 import pytest
 import pydantic
 
-from src.characters import abilities
+from characters import ability
 
 class TestAbility():
         
     @pytest.fixture
-    def ability_fixture(self) -> abilities.Ability:
-        return abilities.Ability(name="Test Ability")
+    def ability_fixture(self) -> ability.Ability:
+        return ability.Ability(name="Test Ability")
 
-    def test_create(self, ability_fixture: abilities.Ability):
+    def test_create(self, ability_fixture: ability.Ability):
         assert(ability_fixture)
     
     def test_create_with_experience(self):
-        assert(abilities.Ability(name="Test Ability", experience=2))
+        assert(ability.Ability(name="Test Ability", experience=2))
     
     def test_create_with_level(self):
-        assert(abilities.Ability(name="Test Ability", experience=2))
+        assert(ability.Ability(name="Test Ability", experience=2))
     
     def test_create_with_level_and_experience(self):
-        assert(abilities.Ability(name="Test Ability", experience=12, level=2))
+        assert(ability.Ability(name="Test Ability", experience=12, level=2))
 
     def test_create_error_negative_experience(self):
         with pytest.raises(pydantic.ValidationError):
-            abilities.Ability(name="Test Ability", experience=-2)
+            ability.Ability(name="Test Ability", experience=-2)
 
     def test_create_error_too_much_experience(self):
         with pytest.raises(pydantic.ValidationError):
-            abilities.Ability(name="Test Ability", experience=12)
+            ability.Ability(name="Test Ability", experience=12)
 
-    def test_add_too_much_experience(self, ability_fixture: abilities.Ability):
+    def test_add_too_much_experience(self, ability_fixture: ability.Ability):
         with pytest.raises(pydantic.ValidationError):
             ability_fixture.experience+=20
 
-    def test_add_experience(self, ability_fixture: abilities.Ability):
+    def test_add_experience(self, ability_fixture: ability.Ability):
         ability_fixture.add_experience(20)
         assert(ability_fixture.level==2)
         assert(ability_fixture.experience==5)
