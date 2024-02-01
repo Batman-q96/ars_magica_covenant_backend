efcc5f0a0362110980ed42f9166faf4a1feaaea2
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-02-01 04:09:51 -0500

    wip on healing tests

--- src/characters/wound_tracker.py
+++ src/characters/wound_tracker.py
@@ -1,8 +1,10 @@
-from typing import Optional, Callable, Sequence
+from __future__ import annotations
+from typing import Optional, Callable, Sequence, ClassVar
 import math
 import enum
 import datetime
 from dateutil import relativedelta
+import abc
 
 import pydantic
 
@@ -13,29 +15,72 @@ class WoundStatus(enum.Enum):
     SAME = 0
     BETTER = 1
 
-class Wound(pydantic.BaseModel):
+class Wound(pydantic.BaseModel, abc.ABC):
     model_config = pydantic.ConfigDict(validate_assignment=True, arbitrary_types_allowed=True)
     bonus: int | None
     status: WoundStatus = WoundStatus.SAME
+    _STABLE_EASE_FACTOR: ClassVar[Optional[int]]
+    _RECOVERY_EASE_FACTOR: ClassVar[Optional[int]]
+    _STABLE_RECOVERY_BONUS: ClassVar[Optional[int]]
     recovery_bonus: int | None = pydantic.Field(default=0, ge=0, multiple_of=3)
     recovery_period: relativedelta.relativedelta | None
-class LightWound(Wound):
-    bonus: int = pydantic.Field(default=-1, init_var=False, frozen=True)
+
+    @abc.abstractmethod
+    def heal(self, recovery_result: int) -> None:
+        pass
+
+class _StandardWound(Wound):
+    bonus: int
+    _STABLE_EASE_FACTOR: ClassVar[int]
+    _RECOVERY_EASE_FACTOR: ClassVar[int]
+    _STABLE_RECOVERY_BONUS: ClassVar[int] = 3
+    recovery_bonus: int = pydantic.Field(default=0, ge=0, multiple_of=3)
+    recovery_period: relativedelta.relativedelta
+
+    def heal(self, recovery_result: int) -> None:
+        if recovery_result < self._STABLE_EASE_FACTOR:
+            self.status = WoundStatus.WORSE
+        elif self._STABLE_EASE_FACTOR <= recovery_result and recovery_result < self._RECOVERY_EASE_FACTOR:
+            self.status = WoundStatus.SAME
+            self.recovery_bonus += self._STABLE_RECOVERY_BONUS
+        elif self._RECOVERY_EASE_FACTOR <= recovery_result:
+            self.status = WoundStatus.BETTER
+
+class LightWound(_StandardWound):
+    bonus: ClassVar[int] = -1
+    _STABLE_EASE_FACTOR: ClassVar[int] = 4
+    _RECOVERY_EASE_FACTOR: ClassVar[int] = 10
+    _STABLE_RECOVERY_BONUS: ClassVar[int] = 3
     recovery_period: datetime.timedelta = pydantic.Field(default=relativedelta.relativedelta(weeks=1), init_var=False, frozen=True)
-class MediumWound(Wound):
-    bonus: int = pydantic.Field(default=-3, init_var=False, frozen=True)
+class MediumWound(_StandardWound):
+    bonus: ClassVar[int] = -3
+    _STABLE_EASE_FACTOR: ClassVar[int] = 6
+    _RECOVERY_EASE_FACTOR: ClassVar[int] = 12
+    _STABLE_RECOVERY_BONUS: ClassVar[int] = 3
     recovery_period: datetime.timedelta = pydantic.Field(default=relativedelta.relativedelta(months=1), init_var=False, frozen=True)
-class HeavyWound(Wound):
-    bonus: int = pydantic.Field(default=-5, init_var=False, frozen=True)
+class HeavyWound(_StandardWound):
+    bonus: ClassVar[int] = -5
+    _STABLE_EASE_FACTOR: ClassVar[int] = 9
+    _RECOVERY_EASE_FACTOR: ClassVar[int] = 15
+    _STABLE_RECOVERY_BONUS: ClassVar[int] = 3
     recovery_period: datetime.timedelta = pydantic.Field(default=relativedelta.relativedelta(months=3), init_var=False, frozen=True)
-class IncapacitatingWound(Wound):
-    bonus: None = pydantic.Field(default=None, init_var=False, frozen=True)
+class IncapacitatingWound(_StandardWound):
+    bonus: ClassVar[None] = None
+    _STABLE_EASE_FACTOR: ClassVar[int] = 0
+    _RECOVERY_EASE_FACTOR: ClassVar[int] = 9
+    _STABLE_RECOVERY_BONUS: ClassVar[int] = -1
     recovery_period: datetime.timedelta = pydantic.Field(default=relativedelta.relativedelta(hours=12), init_var=False, frozen=True)
     recovery_bonus: int = pydantic.Field(default=0, le=0)
 class DeadlyWound(Wound):
-    bonus: None = pydantic.Field(default=None, init_var=False, frozen=True)
+    bonus: ClassVar[None] = None
+    _STABLE_EASE_FACTOR: ClassVar[None] = None
+    _RECOVERY_EASE_FACTOR: ClassVar[None] = None
+    _STABLE_RECOVERY_BONUS: ClassVar[None] = None
     recovery_bonus: None = pydantic.Field(default=None, init_var=False, frozen=True)
     recovery_period: None = pydantic.Field(default=None, init_var=False, frozen=True)
+
+    def heal(self, recovery_result: int) -> None:
+        raise HealingDeathException()
 
 class HealingDeathException(Exception):
     pass
@@ -146,40 +191,8 @@ class WoundTracker(pydantic.BaseModel):
         else:
             raise ValueError
     
-    def _recover_wound(self,
-            wound: Wound,
-            recovery_result: int,
-            stable_ease_factor: int,
-            improvement_ease_factor: int,
-            wound_recovery_bonus: int
-        ) -> None:
-        if wound.recovery_bonus is None:
-            raise HealingDeathException
-        if recovery_result < stable_ease_factor:
-            wound.status = WoundStatus.WORSE
-        elif stable_ease_factor <= recovery_result and recovery_result < improvement_ease_factor:
-            wound.status = WoundStatus.SAME
-            wound.recovery_bonus += wound_recovery_bonus
-        elif improvement_ease_factor <= recovery_result:
-            wound.status = WoundStatus.BETTER
-        else:
-            raise ValueError
-        
-    def _recover_light_wound(self, wound: LightWound, recovery_result: int):
-        self._recover_wound(wound, recovery_result, 4, 10, 3)
-
-    def _recover_medium_wound(self, wound: LightWound, recovery_result: int):
-        self._recover_wound(wound, recovery_result, 6, 12, 3)
-
-    def _recover_heavy_wound(self, wound: LightWound, recovery_result: int):
-        self._recover_wound(wound, recovery_result, 9, 15, 3)
-
-    def _recover_incapacitating_wound(self, wound: IncapacitatingWound, recovery_result: int):
-        self._recover_wound(wound, recovery_result, 0, 9, -1)
-    
     def _recover_all_wounds_of_one_type(self,
             wound_list: Sequence[Wound],
-            wound_recovery_function: Callable,
             wound_got_better_function: Callable,
             wound_got_worse_function: Callable,
             recovery_bonus: int,
@@ -193,7 +206,7 @@ class WoundTracker(pydantic.BaseModel):
             try:
                 if roll_result is None:
                     roll_result = am5_rolls.roll_stress()
-                wound_recovery_function(wound, roll_result+recovery_bonus)
+                wound.heal(roll_result+recovery_bonus)
                 if wound.status == WoundStatus.BETTER:
                     wound_got_better_function()
                 elif wound.status == WoundStatus.WORSE:
@@ -209,7 +222,6 @@ class WoundTracker(pydantic.BaseModel):
         ):
         remaining_light_wounds = self._recover_all_wounds_of_one_type(
             self._light_wounds,
-            self._recover_light_wound,
             lambda : (),
             self._add_medium_wound,
             recovery_bonus,
@@ -225,7 +237,6 @@ class WoundTracker(pydantic.BaseModel):
         ):
         remaining_medium_wounds = self._recover_all_wounds_of_one_type(
             self._medium_wounds,
-            self._recover_medium_wound,
             self._add_light_wound,
             self._add_heavy_wound,
             recovery_bonus,
@@ -239,7 +250,6 @@ class WoundTracker(pydantic.BaseModel):
         ):
         remaining_heavy_wounds = self._recover_all_wounds_of_one_type(
             self._heavy_wounds,
-            self._recover_medium_wound,
             self._add_medium_wound,
             self._add_incapacitating_wound,
             recovery_bonus,
@@ -254,12 +264,11 @@ class WoundTracker(pydantic.BaseModel):
         if self._incapacitating_wound:
             self._recover_all_wounds_of_one_type(
                 [self._incapacitating_wound],
-                self._recover_incapacitating_wound,
                 self._add_heavy_wound,
                 self._add_deadly_wound,
                 recovery_bonus,
                 recovery_roll_results
             )
 
-f = WoundTracker(5)
+f = LightWound()
 print(f)
--- tests/characters/test_wound_tracker.py
+++ tests/characters/test_wound_tracker.py
@@ -1,4 +1,4 @@
-from typing import Literal, Callable
+from typing import Literal, Callable, Tuple
 
 import pytest
 
@@ -33,20 +33,128 @@ class TestWounds():
     def test_light_wound_bonus(self, light_wound_fixture: wound_tracker.LightWound):
         assert light_wound_fixture.bonus == -1
     
+    @pytest.fixture
+    def healed_light_wound_fixture(self, light_wound_fixture: wound_tracker.LightWound, recovery_result: int):
+        light_wound_fixture.heal(recovery_result)
+        return light_wound_fixture
+    @pytest.mark.parametrize("recovery_result, expected_wound_condition", [
+        (wound_tracker.LightWound._STABLE_EASE_FACTOR-1, wound_tracker.WoundStatus.WORSE),
+        (wound_tracker.LightWound._STABLE_EASE_FACTOR, wound_tracker.WoundStatus.SAME),
+        (wound_tracker.LightWound._RECOVERY_EASE_FACTOR, wound_tracker.WoundStatus.BETTER),
+        (3, wound_tracker.WoundStatus.WORSE),
+        (4, wound_tracker.WoundStatus.SAME),
+        (10, wound_tracker.WoundStatus.BETTER),
+    ])
+    def test_light_wound_healing(self,
+            healed_light_wound_fixture: wound_tracker.LightWound,
+            expected_wound_condition: wound_tracker.WoundStatus
+        ):
+        assert healed_light_wound_fixture.status == expected_wound_condition
+    @pytest.mark.parametrize("recovery_result, expected_bonus", [
+        (wound_tracker.LightWound._STABLE_EASE_FACTOR, wound_tracker.LightWound._STABLE_RECOVERY_BONUS),
+        (wound_tracker.LightWound._STABLE_EASE_FACTOR, 3),
+    ])
+    def test_light_wound_stable_bonus(self,
+            healed_light_wound_fixture: wound_tracker.LightWound,
+            expected_bonus: int
+        ):
+        assert healed_light_wound_fixture.recovery_bonus == expected_bonus
+    
     def test_medium_wound_period(self, medium_wound_fixture: wound_tracker.MediumWound):
         assert medium_wound_fixture.recovery_period == relativedelta.relativedelta(months=1)
     def test_medium_wound_bonus(self, medium_wound_fixture: wound_tracker.MediumWound):
         assert medium_wound_fixture.bonus == -3
+    
+    @pytest.fixture
+    def healed_medium_wound_fixture(self, medium_wound_fixture: wound_tracker.MediumWound, recovery_result: int):
+        medium_wound_fixture.heal(recovery_result)
+        return medium_wound_fixture
+    @pytest.mark.parametrize("recovery_result, expected_wound_condition", [
+        (wound_tracker.MediumWound._STABLE_EASE_FACTOR-1, wound_tracker.WoundStatus.WORSE),
+        (wound_tracker.MediumWound._STABLE_EASE_FACTOR, wound_tracker.WoundStatus.SAME),
+        (wound_tracker.MediumWound._RECOVERY_EASE_FACTOR, wound_tracker.WoundStatus.BETTER),
+        (5, wound_tracker.WoundStatus.WORSE),
+        (6, wound_tracker.WoundStatus.SAME),
+        (12, wound_tracker.WoundStatus.BETTER),
+    ])
+    def test_medium_wound_healing(self,
+            healed_medium_wound_fixture: wound_tracker.MediumWound,
+            expected_wound_condition: wound_tracker.WoundStatus
+        ):
+        assert healed_medium_wound_fixture.status == expected_wound_condition
+    @pytest.mark.parametrize("recovery_result, expected_bonus", [
+        (wound_tracker.MediumWound._STABLE_EASE_FACTOR, wound_tracker.LightWound._STABLE_RECOVERY_BONUS),
+        (wound_tracker.MediumWound._STABLE_EASE_FACTOR, 3),
+    ])
+    def test_medium_wound_stable_bonus(self,
+            healed_medium_wound_fixture: wound_tracker.LightWound,
+            expected_bonus: int
+        ):
+        assert healed_medium_wound_fixture.recovery_bonus == expected_bonus
     
     def test_heavy_wound_period(self, heavy_wound_fixture: wound_tracker.HeavyWound):
         assert heavy_wound_fixture.recovery_period == relativedelta.relativedelta(months=3)
     def test_heavy_wound_bonus(self, heavy_wound_fixture: wound_tracker.HeavyWound):
         assert heavy_wound_fixture.bonus == -5
     
+    @pytest.fixture
+    def healed_heavy_wound_fixture(self, heavy_wound_fixture: wound_tracker.HeavyWound, recovery_result: int):
+        heavy_wound_fixture.heal(recovery_result)
+        return heavy_wound_fixture
+    @pytest.mark.parametrize("recovery_result, expected_wound_condition", [
+        (wound_tracker.HeavyWound._STABLE_EASE_FACTOR-1, wound_tracker.WoundStatus.WORSE),
+        (wound_tracker.HeavyWound._STABLE_EASE_FACTOR, wound_tracker.WoundStatus.SAME),
+        (wound_tracker.HeavyWound._RECOVERY_EASE_FACTOR, wound_tracker.WoundStatus.BETTER),
+        (8, wound_tracker.WoundStatus.WORSE),
+        (9, wound_tracker.WoundStatus.SAME),
+        (15, wound_tracker.WoundStatus.BETTER),
+    ])
+    def test_heavy_wound_healing(self,
+            healed_heavy_wound_fixture: wound_tracker.HeavyWound,
+            expected_wound_condition: wound_tracker.WoundStatus
+        ):
+        assert healed_heavy_wound_fixture.status == expected_wound_condition
+    @pytest.mark.parametrize("recovery_result, expected_bonus", [
+        (wound_tracker.HeavyWound._STABLE_EASE_FACTOR, wound_tracker.HeavyWound._STABLE_RECOVERY_BONUS),
+        (wound_tracker.HeavyWound._STABLE_EASE_FACTOR, 3),
+    ])
+    def test_heavy_wound_stable_bonus(self,
+            healed_heavy_wound_fixture: wound_tracker.HeavyWound,
+            expected_bonus: int
+        ):
+        assert healed_heavy_wound_fixture.recovery_bonus == expected_bonus
+    
     def test_incapacitating_wound_period(self, incapacitating_wound_fixture: wound_tracker.IncapacitatingWound):
         assert incapacitating_wound_fixture.recovery_period == relativedelta.relativedelta(hours=12)
     def test_incapacitating_wound_bonus(self, incapacitating_wound_fixture: wound_tracker.IncapacitatingWound):
         assert incapacitating_wound_fixture.bonus == None
+    
+    @pytest.fixture
+    def healed_incapaciatated_wound_fixture(self, incapacitating_wound_fixture: wound_tracker.IncapacitatingWound, recovery_result: int):
+        incapacitating_wound_fixture.heal(recovery_result)
+        return incapacitating_wound_fixture
+    @pytest.mark.parametrize("recovery_result, expected_wound_condition", [
+        (wound_tracker.IncapacitatingWound._STABLE_EASE_FACTOR-1, wound_tracker.WoundStatus.WORSE),
+        (wound_tracker.IncapacitatingWound._STABLE_EASE_FACTOR, wound_tracker.WoundStatus.SAME),
+        (wound_tracker.IncapacitatingWound._RECOVERY_EASE_FACTOR, wound_tracker.WoundStatus.BETTER),
+        (-1, wound_tracker.WoundStatus.WORSE),
+        (0, wound_tracker.WoundStatus.SAME),
+        (9, wound_tracker.WoundStatus.BETTER),
+    ])
+    def test_incapacitating_wound_healing(self,
+            healed_incapaciatated_wound_fixture: wound_tracker.IncapacitatingWound,
+            expected_wound_condition: wound_tracker.WoundStatus
+        ):
+        assert healed_incapaciatated_wound_fixture.status == expected_wound_condition
+    @pytest.mark.parametrize("recovery_result, expected_bonus", [
+        (wound_tracker.IncapacitatingWound._STABLE_EASE_FACTOR, wound_tracker.IncapacitatingWound._STABLE_RECOVERY_BONUS),
+        (wound_tracker.IncapacitatingWound._STABLE_EASE_FACTOR, -1),
+    ])
+    def test_incapacitating_wound_stable_bonus(self,
+            healed_incapaciatated_wound_fixture: wound_tracker.IncapacitatingWound,
+            expected_bonus: int
+        ):
+        assert healed_incapaciatated_wound_fixture.recovery_bonus == expected_bonus
 
     def test_deadly_wound_period(self, deadly_wound_fixture: wound_tracker.DeadlyWound):
         assert deadly_wound_fixture.recovery_period == None
@@ -56,60 +164,103 @@ class TestWounds():
         assert deadly_wound_fixture.recovery_bonus == None
 
 class TestWoundTracker():
+    WOUND_FIXTURE_SIZE = 5
+    WOUND_FIELDS_ARRAY = [
+        "light_wounds",
+        "medium_wounds",
+        "heavy_wounds",
+        "incapacitated",
+        "dead",
+    ]
+
     @pytest.fixture
     def wound_tracker_fixture(self) -> wound_tracker.WoundTracker:
-        return wound_tracker.WoundTracker(size=0)
-
-    baseline_wounds = [# what does the wound tracker look like at baseline
-        # field name    baseline value
-        ("light_wounds", 0),
-        ("medium_wounds", 0),
-        ("heavy_wounds", 0),
-        ("incapacitated", False),
-        ("dead", False),
-    ]
-    altered_wounds = [# what do we expect to chagne in the wound tracker
-        # field_to_check    expected_value  penalty     damage
-        ("light_wounds", 1, -1, 5),
-        ("medium_wounds", 1, -3, 10),
-        ("heavy_wounds", 1, -5, 15),
-        ("incapacitated", True, None, 20),
-        ("dead", True, None, 25)
-    ]
+        # subtract 5 because it gets normalized already in the wound tracker
+        return wound_tracker.WoundTracker(size=self.WOUND_FIXTURE_SIZE-5)
     
     @pytest.fixture
-    def wounded_fixture(self, wound_tracker_fixture: wound_tracker.WoundTracker, damage: int):
-        wound_tracker_fixture.take_damage(damage)
+    def wound_penalty_test_fixture(self, wound_tracker_fixture: wound_tracker.WoundTracker):
+        wound_tracker_fixture._add_light_wound()
+        wound_tracker_fixture._add_medium_wound()
+        wound_tracker_fixture._add_heavy_wound()
         return wound_tracker_fixture
-    @pytest.mark.parametrize("field_to_check, expected_value, penalty, damage", altered_wounds)
+    
+    @pytest.mark.parametrize("expected_penalty", [
+        wound_tracker.HeavyWound.bonus+wound_tracker.MediumWound.bonus+wound_tracker.LightWound.bonus,
+        -9
+    ])
+    def test_wound_tracker_penalty(self,
+            wound_penalty_test_fixture: wound_tracker.WoundTracker,
+            expected_penalty: int
+        ):
+        assert wound_penalty_test_fixture.wound_bonus == expected_penalty
+
+    baseline_wounds = zip(WOUND_FIELDS_ARRAY, [# what does the wound tracker look like at baseline
+        #light, medium, heavy, incap, dead
+        0, 0, 0, False, False,
+    ])
+    
+    @pytest.fixture(params=zip(WOUND_FIELDS_ARRAY, [
+        WOUND_FIXTURE_SIZE,
+        WOUND_FIXTURE_SIZE*2,
+        WOUND_FIXTURE_SIZE*3,
+        WOUND_FIXTURE_SIZE*4,
+        WOUND_FIXTURE_SIZE*5,
+    ]))
+    def wounded_fixture_tuple(self, wound_tracker_fixture: wound_tracker.WoundTracker, request: pytest.FixtureRequest):
+        wound_tracker_fixture.take_damage(request.param[1])
+        return wound_tracker_fixture, request.param[0]
     @pytest.mark.parametrize("baseline_field, baseline_value", baseline_wounds)
     def test_wounding_no_extra_wounds(self,
-            wounded_fixture: wound_tracker.WoundTracker,
-            field_to_check: str,
-            expected_value: int | Literal[True],
-            penalty: int | None,
-            damage: int,
+            wounded_fixture_tuple: Tuple[wound_tracker.WoundTracker, str],
             baseline_field: str,
-            baseline_value: int | Literal[False]):
-        if field_to_check == baseline_field:
+            baseline_value: int | Literal[False]
+        ):
+        wounded_fixture, wound_field = wounded_fixture_tuple
+        if baseline_field == wound_field:
             pass
         else:
             assert wounded_fixture.model_computed_fields[baseline_field].wrapped_property.fget(wounded_fixture) == baseline_value
-    @pytest.mark.parametrize("field_to_check, expected_value, penalty, damage", altered_wounds)
     def test_wounding_correct_wound_added(self,
-            wounded_fixture: wound_tracker.WoundTracker,
-            field_to_check: str,
-            expected_value: int | Literal[True],
-            penalty: int | None,
-            damage: int
+            wounded_fixture_tuple: Tuple[wound_tracker.WoundTracker, str],
         ):
-        assert wounded_fixture.model_computed_fields[field_to_check].wrapped_property.fget(wounded_fixture) == expected_value
-    @pytest.mark.parametrize("field_to_check, expected_value, penalty, damage", altered_wounds)
-    def test_wounding_correct_penalty(self,
-            wounded_fixture: wound_tracker.WoundTracker,
-            field_to_check: str,
-            expected_value: int | Literal[True],
-            penalty: int | None,
-            damage: int,
+        wounded_fixture, wound_field = wounded_fixture_tuple
+        assert wounded_fixture.model_computed_fields[wound_field].wrapped_property.fget(wounded_fixture)
+
+    fully_wounded = zip(WOUND_FIELDS_ARRAY, [# what does the fully wounded wound tracker look like
+        #light, medium, heavy, incap, dead
+        1, 1, 1, True, True,
+    ])
+
+    def fully_wounded_fixture(self, wound_tracker_fixture: wound_tracker.WoundTracker):
+        wound_tracker_fixture._add_light_wound()
+        wound_tracker_fixture._add_medium_wound()
+        wound_tracker_fixture._add_heavy_wound()
+        wound_tracker_fixture._add_incapacitating_wound()
+        wound_tracker_fixture._add_deadly_wound()
+        return wound_tracker_fixture
+    
+    @pytest.mark.parametrize("recovery_result", [
+        wound_tracker.LightWound._STABLE_EASE_FACTOR-1,
+        wound_tracker.LightWound._STABLE_EASE_FACTOR,
+        wound_tracker.LightWound._RECOVERY_EASE_FACTOR,
+        3,
+        4,
+        10
+    ])
+    def test_light_wound_healing(self,
+            fully_wounded_fixture: wound_tracker.WoundTracker,
+            recovery_result: int
         ):
-        assert wounded_fixture.wound_bonus == penalty
+        fully_wounded_fixture.recover_all_light_wounds(recovery_result)
+        
+    
+
+
+    # @pytest.mark.parametrize("baseline_field, baseline_value", baseline_wounds)
+    # def test_wound_healing_correct_baseline_wounds(self,
+    #         healed_fixture: wound_tracker.WoundTracker,
+    #         baseline_field: str,
+    #         baseline_value: int | Literal[False]
+    #     ):
+    #     assert healed_fixture.model_computed_fields[baseline_field].wrapped_property.fget(healed_fixture) == baseline_value
