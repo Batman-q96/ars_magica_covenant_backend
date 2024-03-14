b9557f672f5164ebbc3fad8383c9c80bbb17a0ca
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-03-13 21:58:08 -0400

    personality trait docstring and format

--- src/characters/personality_trait.py
+++ src/characters/personality_trait.py
@@ -1,5 +1,10 @@
+"""Personality traits are descriptors that help guide roleplaying a character ArM5(18)"""
+
 import pydantic
 
+
 class PersonalityTrait(pydantic.BaseModel):
+    """Class to define a personality trait"""
+
     name: str
     intensity: int = pydantic.Field(ge=-3, le=3)
--- tests/characters/test_personality_trait.py
+++ tests/characters/test_personality_trait.py
@@ -1,11 +1,18 @@
+"""Tests for personality traits"""
+
 import pytest
 
-from characters import personality_trait
+from src.characters import personality_trait
 
-class TestPersonalityTrait():
+
+class TestPersonalityTrait:
+    """Tests for the personality trait class implementation"""
+
     @pytest.fixture
     def personality_trait_fixture(self) -> personality_trait.PersonalityTrait:
+        """Base fixture for further personality trait tests"""
         return personality_trait.PersonalityTrait(name="Test trait", intensity=-2)
-    
+
     def test_create(self, personality_trait_fixture):
-        assert(personality_trait_fixture)
+        """Test successful creation of personality traits"""
+        assert personality_trait_fixture
