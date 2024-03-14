c42f9125be96d9baa4b93c8dd31bb7de1343a035
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-03-14 09:29:23 -0400

    added new funcs and tests

--- src/characters/parts/characterstics.py
+++ src/characters/parts/characterstics.py
@@ -1,6 +1,14 @@
 """Characterstics are numerical stats describing ars magica characters see ArM5(18)"""
 
+from typing import Self, Generator
+import random
 import pydantic
+
+_STAT_COSTS = {-3: -6, -2: -3, -1: -1, 0: 0, 1: 1, 2: 3, 3: 6}
+
+
+class ExtremeCharactersticsError(Exception):
+    """Exception raised when characterstics are out of expected bounds"""
 
 
 class Characteristics(pydantic.BaseModel):
@@ -14,3 +22,76 @@ class Characteristics(pydantic.BaseModel):
     perception: int = 0
     presence: int = 0
     communication: int = 0
+
+    def __add__(self, add_val: int) -> Self:
+        results_dict = {}
+        for char_name, value in self:
+            results_dict[char_name] = value + add_val
+
+        return self.__class__(**results_dict)
+
+    def __sub__(self, sub_val: int) -> Self:
+        results_dict = {}
+        for char_name, value in self:
+            results_dict[char_name] = value - sub_val
+
+        return self.__class__(**results_dict)
+
+    @classmethod
+    def iter_char_names(cls) -> Generator[str, None, None]:
+        """Iterate to get just the names of the characterstics"""
+        yield "strength"
+        yield "stamina"
+        yield "quickness"
+        yield "dexterity"
+        yield "intelligence"
+        yield "perception"
+        yield "presence"
+        yield "communication"
+
+    def __iter__(self) -> Generator[tuple[str, int], None, None]:
+        yield "strength", self.strength
+        yield "stamina", self.stamina
+        yield "quickness", self.quickness
+        yield "dexterity", self.dexterity
+        yield "intelligence", self.intelligence
+        yield "perception", self.perception
+        yield "presence", self.presence
+        yield "communication", self.communication
+
+    @classmethod
+    def generate_fully_random(cls, rand_min: int = -3, rand_max: int = 3) -> Self:
+        """Generate a fully random set of stats"""
+        return cls(
+            **{
+                characteristic: random.randint(rand_min, rand_max)
+                for characteristic in cls.iter_char_names()
+            }
+        )
+
+    def get_characterstics_point_cost(self) -> int:
+        """Determine the cost of these characterstics as if they were made in character creation"""
+
+        running_total = 0
+        try:
+            for _, value in self:
+                running_total += _STAT_COSTS[value]
+            return running_total
+        except KeyError as exc:
+            raise ExtremeCharactersticsError from exc
+
+    def is_valid_starting_characterstics(self, max_cost: int = 7) -> bool:
+        """Check if the total cost of these characterstics is below the max"""
+        try:
+            return self.get_characterstics_point_cost() <= max_cost
+        except ExtremeCharactersticsError:
+            return False
+
+    def is_valid_starting_characterstics_and_fully_spent(
+        self, max_cost: int = 7
+    ) -> bool:
+        """Check if the total cost of these characterstics is exactly at the max"""
+        try:
+            return self.get_characterstics_point_cost() == max_cost
+        except ExtremeCharactersticsError:
+            return False
--- tests/characters/parts/test_characterstics.py
+++ tests/characters/parts/test_characterstics.py
@@ -16,3 +16,121 @@ class TestCharacterstics:
     def test_create(self, characteristic_fixture):
         """Test characterstic creation"""
         assert characteristic_fixture
+
+    @pytest.mark.parametrize(
+        "char_name",
+        [
+            "strength",
+            "stamina",
+            "quickness",
+            "dexterity",
+            "intelligence",
+            "perception",
+            "presence",
+            "communication",
+        ],
+    )
+    def test_iterate_names(self, char_name: str):
+        """Make sure we return every characterstic when iterating over the names"""
+        assert char_name in characterstics.Characteristics.iter_char_names()
+
+    def test_iterate_over_fixture(
+        self, characteristic_fixture: characterstics.Characteristics
+    ):
+        """Make sure all the characterstics were properly set to 0"""
+        for _, value in characteristic_fixture:
+            assert value == 0
+
+    @pytest.mark.parametrize(
+        ("values", "expected_value"), [([3], 6), ([1, 2], 4), ([3, 1], 7)]
+    )
+    def test_get_characterstics_point_cost(
+        self,
+        characteristic_fixture: characterstics.Characteristics,
+        values: list[int],
+        expected_value: int,
+    ):
+        """Make sure we can properly calculate point costs"""
+        for value, (char_name, _) in zip(values, characteristic_fixture):
+            characteristic_fixture.__dict__[char_name] = value
+        assert characteristic_fixture.get_characterstics_point_cost() == expected_value
+
+    @pytest.mark.parametrize(
+        ("values", "target_value", "expected_value"),
+        [
+            ([3], 5, False),
+            ([3], 7, True),
+            ([3, 1], 7, True),
+            ([3, 1], 5, False),
+            ([-1, 1, 1, 1], 4, True),
+            ([1, 1, 1, 1], 2, False),
+            ([9, 1, 1, 1], 2, False),
+        ],
+    )
+    def test_is_valid_starting_characterstics(
+        self,
+        characteristic_fixture: characterstics.Characteristics,
+        values: list[int],
+        target_value: int,
+        expected_value: bool,
+    ):
+        """Test that we can properly determine if these characterstics are valid"""
+
+        for value, (char_name, _) in zip(values, characteristic_fixture):
+            characteristic_fixture.__dict__[char_name] = value
+
+        assert (
+            characteristic_fixture.is_valid_starting_characterstics(
+                max_cost=target_value
+            )
+            == expected_value
+        )
+
+    @pytest.mark.parametrize(
+        ("values", "target_value", "expected_value"),
+        [
+            ([3], 5, False),
+            ([3], 7, False),
+            ([3, 1], 7, True),
+            ([3, 1], 8, False),
+            ([1, 1, 1, 1], 4, True),
+            ([1, 1, -1, 1], 2, True),
+            ([-1, 1, -10, 1], 2, False),
+        ],
+    )
+    def test_is_valid_starting_characterstics_and_fully_spent(
+        self,
+        characteristic_fixture: characterstics.Characteristics,
+        values: list[int],
+        target_value: int,
+        expected_value: bool,
+    ):
+        """Test that we can properly determine if these characterstics are valid"""
+
+        for value, (char_name, _) in zip(values, characteristic_fixture):
+            characteristic_fixture.__dict__[char_name] = value
+
+        assert (
+            characteristic_fixture.is_valid_starting_characterstics_and_fully_spent(
+                max_cost=target_value
+            )
+            == expected_value
+        )
+
+    def test_raises_extreme_characterstics_error(
+        self, characteristic_fixture: characterstics.Characteristics
+    ):
+        """Test that we raise the extreme characterstics error on extreme values as expected"""
+        characteristic_fixture.strength = 4
+        with pytest.raises(characterstics.ExtremeCharactersticsError):
+            characteristic_fixture.get_characterstics_point_cost()
+
+    def test_add(self, characteristic_fixture: characterstics.Characteristics):
+        """Test that we can add values to all characterstics"""
+        for _, val in characteristic_fixture + 1:
+            assert val == 1
+
+    def test_sub(self, characteristic_fixture: characterstics.Characteristics):
+        """Test that we can subtract values from all characterstics"""
+        for _, val in characteristic_fixture - 1:
+            assert val == -1
