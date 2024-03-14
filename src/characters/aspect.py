135bec0ea36cd6dd0bb856d7373481ca21386f57
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-03-13 21:33:00 -0400

    updated docstrings and formatting

--- src/characters/aspect.py
+++ src/characters/aspect.py
@@ -1,15 +1,26 @@
-from typing import Literal, Iterable
+"""Aspects are virtues and flaws see ArM5(36-61)"""
+
+from typing import Literal
 
 import pydantic
 
+
 class Aspect(pydantic.BaseModel):
+    """The general base class for both virtues and flaws"""
+
     name: str
     scale: Literal["Minor", "Major"]
     tag: Literal["Hermetic", "Supernatural", "Social Status", "General"]
     content: str
 
+
 class Virtue(Aspect):
-    pass
+    """Virtues are positive aspects"""
+
 
 class Flaw(Aspect):
-    tag: Literal["Hermetic", "Personality", "Story", "Supernatural", "Social Status", "General"]
+    """Flaws are negative aspects"""
+
+    tag: Literal[
+        "Hermetic", "Personality", "Story", "Supernatural", "Social Status", "General"
+    ]
--- tests/characters/test_aspects.py
+++ tests/characters/test_aspects.py
@@ -1,19 +1,44 @@
+"""Tests for aspects"""
+
 import pytest
 
-from characters import aspect
+from src.characters import aspect
 
-class TestAspect():
+
+class TestAspect:
+    """Base Aspect tests"""
+
     @pytest.fixture
     def test_aspect_fixture(self) -> aspect.Aspect:
+        """Aspect fixture for testing"""
         return aspect.Aspect(scale="Major", tag="General", content="", name="Test")
-    
+
     def test_create(self, test_aspect_fixture):
-        assert(test_aspect_fixture)
-    
-class TestFlaw():
+        """Test creation of the base fixture"""
+        assert test_aspect_fixture
+
+
+class TestVirtue:
+    """Virtue specific tests"""
+
+    @pytest.fixture
+    def test_virtue_fixture(self) -> aspect.Virtue:
+        """Virtue fixture for testing"""
+        return aspect.Virtue(scale="Major", tag="General", content="", name="Test")
+
+    def test_create(self, test_virtue_fixture):
+        """Test creation of the virtue fixture"""
+        assert test_virtue_fixture
+
+
+class TestFlaw:
+    """Flaw specific tests"""
+
     @pytest.fixture
     def test_flaw_fixture(self) -> aspect.Flaw:
+        """Base flaw fixture for testing"""
         return aspect.Flaw(scale="Major", tag="General", content="", name="Test")
-    
+
     def test_create(self, test_flaw_fixture):
-        assert(test_flaw_fixture)
+        """Test creation of the base flaw fixture"""
+        assert test_flaw_fixture
