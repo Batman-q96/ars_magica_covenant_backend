f6ea547bf7d4e5a97d1a30fa472fa8667d0580a5
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-03-13 22:02:24 -0400

    add docstring and tracker

--- src/characters/reputation.py
+++ src/characters/reputation.py
@@ -1,7 +1,12 @@
-from __future__ import annotations
+"""Reputation is how well known you are ArM5(19)"""
+
+from typing import Self
 import pydantic
 
+
 class Reputation(pydantic.BaseModel):
+    """Reputation implementation class"""
+
     model_config = pydantic.ConfigDict(validate_assignment=True)
     score: int = pydantic.Field(default=1, ge=1)
     deeds: int = pydantic.Field(default=0, ge=0)
@@ -9,13 +14,16 @@ class Reputation(pydantic.BaseModel):
     target: str
 
     @pydantic.model_validator(mode="after")
-    def max_deeds(self) -> Reputation:
-        assert(self.deeds < 5*(self.score+1))
+    def max_deeds(self) -> Self:
+        """Validator to make sure reputable deeds never exceed those allowed by
+        the current reputation score"""
+        assert self.deeds < 5 * (self.score + 1)
         return self
 
     def add_deeds(self, deeds_gained: int) -> None:
+        """Function to safely add deeds and increase reputation score if needed"""
         new_deeds = self.deeds + deeds_gained
-        while new_deeds >= 5*(self.score+1):
+        while new_deeds >= 5 * (self.score + 1):
             self.score += 1
-            new_deeds = new_deeds - 5*self.score
+            new_deeds = new_deeds - 5 * self.score
         self.deeds = new_deeds
--- tests/characters/test_reputation.py
+++ tests/characters/test_reputation.py
@@ -1,31 +1,44 @@
+"""Tests for reputation"""
+
 import pytest
 
 import pydantic
 
 
-from characters import reputation
+from src.characters import reputation
 
-class TestReputation():
+
+class TestReputation:
+    """Tests for the reputation class implementaiton"""
+
     @pytest.fixture
     def reputation_fixture(self) -> reputation.Reputation:
+        """Base fixture for reputaiton testing"""
         return reputation.Reputation(content="Testing", target="Everyone")
-    
+
     def test_create(self, reputation_fixture):
-        assert(reputation_fixture)
+        """Test creation of the base reputation fixutre"""
+        assert reputation_fixture
 
     def test_create_error_negative_score(self):
+        """Test that we can't create a reputation with negative score"""
         with pytest.raises(pydantic.ValidationError):
             reputation.Reputation(score=-1, content="Testing", target="Everyone")
-    
+
     def test_create_error_too_many_deeds(self):
+        """Test taht we can't create a reputation with deeds exceeding the score"""
         with pytest.raises(pydantic.ValidationError):
-            reputation.Reputation(score=1, deeds=10, content="Testing", target="Everyone")
+            reputation.Reputation(
+                score=1, deeds=10, content="Testing", target="Everyone"
+            )
 
     def test_add_too_many_deeds(self, reputation_fixture: reputation.Reputation):
+        """Test that we can't directly add deeds to exceed the reputation score"""
         with pytest.raises(pydantic.ValidationError):
-            reputation_fixture.deeds+=10
+            reputation_fixture.deeds += 10
 
     def test_add_reputation_properly(self, reputation_fixture: reputation.Reputation):
+        """Test that adding in deeds correctly will adjust the repuation score as desired"""
         reputation_fixture.add_deeds(15)
-        assert(reputation_fixture.score == 2)
-        assert(reputation_fixture.deeds == 5)
+        assert reputation_fixture.score == 2
+        assert reputation_fixture.deeds == 5
