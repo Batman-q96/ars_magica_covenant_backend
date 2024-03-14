dcc5f044e5f8efdb271a2f1fc9589eb3a22b5e48
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-03-13 21:18:20 -0400

    added abilities and tests

--- src/characters/ability.py
+++ src/characters/ability.py
@@ -1,20 +1,28 @@
-from __future__ import annotations
+"""Abilities are skills that characters have, both magical and non magical see ArM5(62-67)"""
+
+# from __future__ import annotations
+from typing import Self
 import pydantic
 
+
 class Ability(pydantic.BaseModel):
+    """Ability implementation"""
+
     model_config = pydantic.ConfigDict(validate_assignment=True)
     name: str
     level: int = pydantic.Field(default=0)
     experience: int = pydantic.Field(default=0, ge=0)
 
     @pydantic.model_validator(mode="after")
-    def max_experience(self) -> Ability:
-        assert(self.experience < 5*(self.level+1))
+    def max_experience(self) -> Self:
+        """Validator to make sure experience isn't too high for level"""
+        assert self.experience < 5 * (self.level + 1)
         return self
 
     def add_experience(self, exp_to_gain: int) -> None:
+        """Safely add experience potentially increasing level if needed"""
         new_experience = self.experience + exp_to_gain
-        while new_experience >= 5*(self.level+1):
+        while new_experience >= 5 * (self.level + 1):
             self.level += 1
-            new_experience = new_experience - 5*self.level
+            new_experience = new_experience - 5 * self.level
         self.experience = new_experience
--- tests/characters/test_ability.py
+++ tests/characters/test_ability.py
@@ -1,39 +1,52 @@
+"""Tests for abilities"""
+
 import pytest
 import pydantic
 
 from characters import ability
 
-class TestAbility():
-        
+
+class TestAbility:
+    """Tests for the ability class"""
+
     @pytest.fixture
     def ability_fixture(self) -> ability.Ability:
+        """Create an ability fixture for testing"""
         return ability.Ability(name="Test Ability")
 
     def test_create(self, ability_fixture: ability.Ability):
-        assert(ability_fixture)
-    
+        """Test creation of an ability"""
+        assert ability_fixture
+
     def test_create_with_experience(self):
-        assert(ability.Ability(name="Test Ability", experience=2))
-    
+        """Test creation with some base experience"""
+        assert ability.Ability(name="Test Ability", experience=2)
+
     def test_create_with_level(self):
-        assert(ability.Ability(name="Test Ability", experience=2))
-    
+        """Test creation with some base level"""
+        assert ability.Ability(name="Test Ability", experience=2)
+
     def test_create_with_level_and_experience(self):
-        assert(ability.Ability(name="Test Ability", experience=12, level=2))
+        """Test creation with some base level and experience"""
+        assert ability.Ability(name="Test Ability", experience=12, level=2)
 
     def test_create_error_negative_experience(self):
+        """Test that we can't create with negative experience"""
         with pytest.raises(pydantic.ValidationError):
             ability.Ability(name="Test Ability", experience=-2)
 
     def test_create_error_too_much_experience(self):
+        """Test that we can't create with experience higher than max allowed by level"""
         with pytest.raises(pydantic.ValidationError):
             ability.Ability(name="Test Ability", experience=12)
 
     def test_add_too_much_experience(self, ability_fixture: ability.Ability):
+        """Test that we can't add too much experience"""
         with pytest.raises(pydantic.ValidationError):
-            ability_fixture.experience+=20
+            ability_fixture.experience += 20
 
     def test_add_experience(self, ability_fixture: ability.Ability):
+        """Test that we can add too much experience using add_experience"""
         ability_fixture.add_experience(20)
-        assert(ability_fixture.level==2)
-        assert(ability_fixture.experience==5)
+        assert ability_fixture.level == 2
+        assert ability_fixture.experience == 5
