b7df5e84478af1699aa27466426eb8b334dd2967
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-03-13 21:56:07 -0400

    update fatigue and tests

--- src/characters/fatigue.py
+++ src/characters/fatigue.py
@@ -1,10 +1,18 @@
+"""Fatigue is tracekd via fatigue levels ArM5(178)"""
+
 from typing import Literal
 import datetime
 
 import pydantic
 
 
+class NoFatigueToRecoverError(Exception):
+    """Exception to be raised when we try to recover but there's no fatigue levels to recover"""
+
+
 class FatigueTracker(pydantic.BaseModel):
+    """Class to track fatigue levels"""
+
     model_config = pydantic.ConfigDict(validate_assignment=True)
     short_term_levels: int = pydantic.Field(default=0, ge=0)
     long_term_levels: int = pydantic.Field(default=0, ge=0)
@@ -12,7 +20,8 @@ class FatigueTracker(pydantic.BaseModel):
     @pydantic.computed_field
     @property
     def bonus(self) -> int | bool:
-        total_fatigue_levels = self.short_term_levels+self.long_term_levels
+        """Cumulative penalty inflicted due to fatigue levels"""
+        total_fatigue_levels = self.short_term_levels + self.long_term_levels
         if total_fatigue_levels <= 1:
             return 0
         elif total_fatigue_levels == 2:
@@ -25,11 +34,14 @@ class FatigueTracker(pydantic.BaseModel):
             return False
         else:
             raise RuntimeError
-    
+
     @pydantic.computed_field
     @property
-    def fatigue_level(self) -> Literal["Fresh", "Winded", "Weary", "Tired", "Dazed", "Unconscious"]:
-        total_fatigue_levels = self.short_term_levels+self.long_term_levels
+    def fatigue_level(
+        self,
+    ) -> Literal["Fresh", "Winded", "Weary", "Tired", "Dazed", "Unconscious"]:
+        """Human understandable fatigue level based on overall level of fatigue"""
+        total_fatigue_levels = self.short_term_levels + self.long_term_levels
         if total_fatigue_levels == 0:
             return "Fresh"
         elif total_fatigue_levels == 1:
@@ -46,11 +58,12 @@ class FatigueTracker(pydantic.BaseModel):
             raise ValueError("Total fatigue levels are negative")
 
     def get_short_term_recovery_time(self) -> datetime.timedelta:
+        """Calculate the time to recover a single fatigue level at the current level"""
         if self.short_term_levels == 0:
             # if we have no short term fatigue levels then we can't recover any
-            raise ValueError
+            raise NoFatigueToRecoverError
         else:
-            total_fatigue_levels = self.short_term_levels+self.long_term_levels
+            total_fatigue_levels = self.short_term_levels + self.long_term_levels
             if total_fatigue_levels == 1:
                 recovery_time = datetime.timedelta(minutes=2)
             elif total_fatigue_levels == 2:
@@ -58,7 +71,7 @@ class FatigueTracker(pydantic.BaseModel):
             elif total_fatigue_levels == 3:
                 recovery_time = datetime.timedelta(minutes=30)
             elif total_fatigue_levels > 3:
-                recovery_time = datetime.timedelta(hours=total_fatigue_levels-3)
+                recovery_time = datetime.timedelta(hours=total_fatigue_levels - 3)
             else:
-                raise ValueError
+                raise NoFatigueToRecoverError
         return recovery_time
--- tests/characters/test_fatigue.py
+++ tests/characters/test_fatigue.py
@@ -1,35 +1,58 @@
+"""Tests for the fatigue tracker"""
+
 import datetime
 
 import pydantic
 import pytest
 
-from characters import fatigue
+from src.characters import fatigue
 
-class TestFatigue():
+
+class TestFatigue:
+    """Overall tests for the fatigue tracker"""
+
     @pytest.fixture
     def fatigue_tracker_fixture(self) -> fatigue.FatigueTracker:
+        """Base fixture upon which to run the fatigue tests"""
         return fatigue.FatigueTracker()
-    
+
     def test_create(self, fatigue_tracker_fixture):
-        assert(fatigue_tracker_fixture)
-    
+        """Test proper creation of the fatigue tracker fixture"""
+        assert fatigue_tracker_fixture
+
     def test_create_error_negative_short(self):
+        """Test that negative short term fatigue levels are impossible"""
         with pytest.raises(pydantic.ValidationError):
             fatigue.FatigueTracker(short_term_levels=-1)
-    
+
     def test_create_error_negative_long(self):
+        """Test that negative long term fatigue levels are impossible"""
         with pytest.raises(pydantic.ValidationError):
             fatigue.FatigueTracker(long_term_levels=-1)
 
-    def test_get_recovery_time_error(self, fatigue_tracker_fixture: fatigue.FatigueTracker):
-        with pytest.raises(ValueError):
+    def test_get_recovery_time_error(
+        self, fatigue_tracker_fixture: fatigue.FatigueTracker
+    ):
+        """Test that a lack of fatigue levels means we error out"""
+        with pytest.raises(fatigue.NoFatigueToRecoverError):
             fatigue_tracker_fixture.get_short_term_recovery_time()
 
     def test_get_recovery_time(self, fatigue_tracker_fixture: fatigue.FatigueTracker):
+        """Test that we properly return the recovery time if we have 1 fatigue level"""
         fatigue_tracker_fixture.short_term_levels += 1
-        assert(fatigue_tracker_fixture.get_short_term_recovery_time() == datetime.timedelta(minutes=2))
+        assert (
+            fatigue_tracker_fixture.get_short_term_recovery_time()
+            == datetime.timedelta(minutes=2)
+        )
 
-    def test_get_recovery_time_with_long_term_levels(self, fatigue_tracker_fixture: fatigue.FatigueTracker):
+    def test_get_recovery_time_with_long_term_levels(
+        self, fatigue_tracker_fixture: fatigue.FatigueTracker
+    ):
+        """Test that we properly return the recovery time if we have both short
+        and long term fatigue levels"""
         fatigue_tracker_fixture.short_term_levels += 1
         fatigue_tracker_fixture.long_term_levels += 1
-        assert(fatigue_tracker_fixture.get_short_term_recovery_time() == datetime.timedelta(minutes=10))
+        assert (
+            fatigue_tracker_fixture.get_short_term_recovery_time()
+            == datetime.timedelta(minutes=10)
+        )
