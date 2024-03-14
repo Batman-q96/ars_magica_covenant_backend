b8dee4888deb3647345b8c15dbf0b26e10aec428
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-03-13 21:33:14 -0400

    updated bio info docstrings and formatting

--- src/characters/biographic_info.py
+++ src/characters/biographic_info.py
@@ -1,12 +1,19 @@
+"""Biographic info is character info that is not mechanically reelvant but is 
+relelvant to the character overall"""
+
 import pydantic
 
+
 class BiographicInfo(pydantic.BaseModel):
-    gender: str = ''
-    nationality: str = ''
-    origin: str = ''
-    religion: str = ''
-    height: str = ''
-    weight: str = ''
-    hair: str = ''
-    eyes: str = ''
-    handedness: str = ''
+    """A container class for biographic info"""
+
+    gender: str = ""
+    nationality: str = ""
+    origin: str = ""
+    religion: str = ""
+    height: str = ""
+    weight: str = ""
+    hair: str = ""
+    eyes: str = ""
+    handedness: str = ""
+    description: str = ""
--- tests/characters/test_biographic_info.py
+++ tests/characters/test_biographic_info.py
@@ -1,12 +1,18 @@
-import pydantic
+"""Tests for biographic info"""
+
 import pytest
 
-from characters import biographic_info
+from src.characters import biographic_info
 
-class TestBiographicInfo():
+
+class TestBiographicInfo:
+    """Tests for the BiographicInfo container class"""
+
     @pytest.fixture
     def biographic_info_fixture(self) -> biographic_info.BiographicInfo:
+        """Basic test fixture for further testing of biographic info"""
         return biographic_info.BiographicInfo()
-    
+
     def test_create(self, biographic_info_fixture):
-        assert(biographic_info_fixture)
+        """Test succesful creation of the base test fixture"""
+        assert biographic_info_fixture
