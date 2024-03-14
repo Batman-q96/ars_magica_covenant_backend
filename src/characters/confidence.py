e89ffbe94fac60a68a930366b0deef6dece2e2cd
Batman (Evan Feldman) <ejosephfeldman@gmail.com>
2024-03-13 21:48:48 -0400

    update confidence and tests

--- src/characters/confidence.py
+++ src/characters/confidence.py
@@ -1,12 +1,18 @@
-from __future__ import annotations
+"""Confidence is a character feature used to control luck see ArM5(19-20)"""
+
+from typing import Self
 import pydantic
 
+
 class Confidence(pydantic.BaseModel):
+    """Confidence points implementation as a class"""
+
     model_config = pydantic.ConfigDict(validate_assignment=True)
     points: int = pydantic.Field(default=0, ge=0)
     max: int = pydantic.Field(default=0, ge=0)
 
     @pydantic.model_validator(mode="after")
-    def limit_confidence(self) -> Confidence:
-        assert(self.points <= self.max)
+    def limit_confidence(self) -> Self:
+        """Validator for confidence points to make sure they don't exceed the max"""
+        assert self.points <= self.max
         return self
--- tests/characters/test_confidence.py
+++ tests/characters/test_confidence.py
@@ -1,36 +1,49 @@
+"""Tests for confidence implementation"""
+
 import pydantic
 import pytest
 
-from characters import confidence
+from src.characters import confidence
 
-class TestConfidence():
+
+class TestConfidence:
+    """Tests for the confidence score class"""
+
     @pytest.fixture
     def confidence_fixture(self) -> confidence.Confidence:
+        """Base test fixture for confidence score tests"""
         return confidence.Confidence()
-    
-    def test_create(self, confidence_fixture):
-        assert(confidence_fixture)
-    
+
+    def test_create(self, confidence_fixture: confidence.Confidence):
+        """Test creation of the confidence score class"""
+        assert confidence_fixture
+
     def test_create_with_max(self):
-        assert(confidence.Confidence(max=3))
+        """Test creation with a non zero max"""
+        assert confidence.Confidence(max=3)
 
     def test_create_with_max_and_points(self):
-        assert(confidence.Confidence(points=3, max=3))
+        """Test creation with non zero max and non zero points"""
+        assert confidence.Confidence(points=3, max=3)
 
     def test_create_error_with_points_only(self):
+        """Test that creating with poitns but not raising max will raise error"""
         with pytest.raises(pydantic.ValidationError):
             confidence.Confidence(points=3)
-    
-    def test_points_below_0(self, confidence_fixture):
+
+    def test_points_below_0(self, , confidence_fixture: confidence.Confidence):
+        """Test that we can't have negative confidence points"""
         with pytest.raises(pydantic.ValidationError):
             confidence_fixture.points -= 2
-    
-    def test_points_above_max(self, confidence_fixture):
+
+    def test_points_above_max(self, confidence_fixture: confidence.Confidence):
+        """Test that we can't raise opints above the max"""
         with pytest.raises(pydantic.ValidationError):
             confidence_fixture.points += 2
-    
-    def test_max_increase(self, confidence_fixture):
+
+    def test_max_increase(self, confidence_fixture: confidence.Confidence):
+        """Test that we can raise max and then raise poitns to the new max"""
         confidence_fixture.max += 2
         confidence_fixture.points += 2
-        assert(confidence_fixture.max == 2)
-        assert(confidence_fixture.points == 2)
+        assert confidence_fixture.max == 2
+        assert confidence_fixture.points == 2
